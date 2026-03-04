81cd2982b1881344c8113a90b13c70dda3050532
jorgeeduardoolmos <jorgeeduardoolmos@gmail.com>
2026-03-04 16:29:24 -0300

    Update footer.py

--- utilidades/footer.py
+++ utilidades/footer.py
@@ -99,6 +99,8 @@ def render_footer():
         </div>
         <div class="col">
             <p>📍 CABA | Leloir | San Pedro</p>
+            <p>📍 Av. Rivadavia 4532 - Galería Alefa (local 10) C1042AAP - C.A.B.A.</p>
+            <p>📍 Del Cimarrón 1846 - 1er Piso - Of. 4 C.P.: 1714 - Parque Leloir Ituzaingo</p>            
             <p>📞 11-4847-6467</p>
         </div>
         <div class="col">
